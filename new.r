6465a74716a56bc3de9ce9a3b94f117748f9950e
Mike Aubakirov <aubakirovma@gmail.com>
2023-05-25 17:54:51 +0700

    r upadate

--- new.r
+++ new.r
@@ -1,3 +1,16 @@
-b <-2 
+a <- 1 
+b <- 1 
+c <- -1
 
-b
+solution_1 <- (-b + sqrt(b^2 - 4*a*c)) / (2*a)
+solution_2 <- (-b - sqrt(b^2 - 4*a*c)) / (2*a)
+
+solution_1 
+solution_2
+
+a <- 3
+b <- 2 
+c <- -1
+
+solution_1 
+solution_2
